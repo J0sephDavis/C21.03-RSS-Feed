7e9e683367ff22ad648c16c29968506e08a8c75d
Joseph Davis <josephdavisjoey@gmail.com>
2023-12-10 11:12:50 -0500

    removed rssContents class. Added feed class. Began attempts at multithreading

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,5 @@
+#include <queue>
+#include <thread>
 #include <csignal> //reference for SIGINT & SIGSEGV
 #include <config.h>
 #include <curlpp/Exception.hpp>
@@ -61,6 +63,7 @@ enum logLevel_t {
 class logger {
 	//https://stackoverflow.com/questions/1008019/how-do-you-implement-the-singleton-design-pattern
 	//Modified from: https://drdobbs.com/cpp/logging-in-c/201804215
+	//TODO: queue with mutext for multiple producers (of input) & one consumer (the logger as it writes)
 	//TODO: have log file update during program runtime, not just when it exits successfully...
 	public:
 		static logger& getInstance(logLevel_t level = logINFO) {
@@ -125,7 +128,9 @@ class logger {
 		}
 	private:
 		logLevel_t loggerLevel;
+		//the actual output stream
 		std::ostringstream os;
+		std::queue<std::ostringstream>  messages;
 	public:
 		//sends a message to the log
 		void send(std::string message, logLevel_t level = logINFO) {
@@ -171,50 +176,41 @@ bool match_title(std::string Regular_Expression, std::string title) {
 }
 
 
+std::string url_to_filename(const std::string url) {
+	std::string fileName = url;
+	const std::string url_start = "https://";
+	auto iterator = fileName.find(url_start);
+	fileName.replace(iterator,url_start.size(),"");
+	iterator = fileName.find('/');
+	while (iterator != std::string::npos) {
+		fileName.replace(iterator,1,"");
+		iterator = fileName.find('/');
+	}
+	return fileName;
+}
 //TODO: Single-respoinsibility, stop making this class handle configs & other functionaility...
 /* prepares to download a file given a url, path, & optionally a file name.
  * If a file name is given, save to downloadPath/fileName
  * If no file name is given, lint the url and make it the filename (removing slashes or other symbols)
 */
-class download_entry {
+class download_base {
 	public:
-		download_entry(std::string url,std::string download_path) :
+		download_base(std::string url,fs::path download_path) :
 			log(logger::getInstance()),
 			url(url)
 		{
-			log.send("No fileName provided, creating from linted url", logTRACE);
-			fileName = url;
-
-			const std::string url_start = "https://";
-			auto iterator = fileName.find(url_start);
-			fileName.replace(iterator,url_start.size(),"");
-			log.send("\tfileName: " + fileName, logDEBUG);
-			iterator = fileName.find('/');
-			while (iterator != std::string::npos) {
-				fileName.replace(iterator,1,"");
-				iterator = fileName.find('/');
-			}
-
-			fileName.insert(0,download_path);
-			log.send("\tfileName: " + fileName, logDEBUG);
-		}
-		download_entry(std::string url,std::string download_path, std::string name_of_file):
-			log(logger::getInstance()),
-			url(url)
-		{
+			filePath = download_path;
 			log.send("Called DOWNLOAD_ENTRY constructor", logTRACE);
-			log.send("called with:\n\turl:" + url + "\n\tdownload_path:" + download_path + "\n\tname:" + name_of_file, logDEBUG);
-			fileName = download_path + name_of_file;
-			log.send("created with:\n\turl:" + url + "\n\tname:" + fileName, logDEBUG);
+			log.send("called with:\n\turl:" + url + "\n\tdownload_path:" + download_path.string(), logDEBUG);
 		};
-		~download_entry() {
-			log.send("DOWNLOAD_ENTRY destroyed with:\n\turl:" + url + "\n\tname:" + fileName, logDEBUG);
+		~download_base() {
+			log.send("DOWNLOAD_ENTRY destroyed",logTRACE);
 		}
 		//downloads a file
 		bool fetch() {
 			log.send("fetch", logTRACE);
-			log.send("url:" + url + "\tfileName: " + fileName, logDEBUG);
-			if(fs::exists(fileName)) {
+			log.send("url:" + url + "\tpath: " + filePath.string(), logDEBUG);
+			if(fs::exists(filePath)) {
 				log.send("FILE ALREADY EXISTS", logWARNING);
 #if !CLOBBER_FLAG
 				return false;
@@ -224,7 +220,7 @@ class download_entry {
 			//this section of code somewhat irks me. Not the curlpp,
 			//but the error handling & that we are using a FILE* rather than an fstream.
 			//Must look into how we can get to an fstream with curlpp write callback
-			FILE* pagefile = fopen(fileName.c_str(), "wb");
+			FILE* pagefile = fopen(filePath.c_str(), "wb");
 			try {
 				//set options, URL, Write callback function, progress output, &c
 				curlpp::Easy request;
@@ -253,58 +249,58 @@ class download_entry {
 			fclose(pagefile);
 			return false;
 		}
-		const std::string getFileName() {
-			return fileName;
+		const fs::path getPath() {
+			return filePath;
 		}
 	private:
 		logger& log;
 		const std::string url;
-		std::string fileName;
+		fs::path filePath;
 };
 //TODO: determine a new name for this class
 /* Update the config node on a successful download, IF title != NULL
 */
-class rssContents : private download_entry {
-	public:
-//download_entry(logger &log, std::string url,std::string download_path, std::string name_of_file = "") :
-		rssContents(rx::xml_node<> &config, char* title, std::string url, std::string download_folder) :
-			download_entry(url, download_folder),
-			log(logger::getInstance()),
-			allocated_title(title),
-			associated_config(config)
-		{
-			log.send("rssContents constructor", logTRACE);
-			if (title != NULL)
-				log.send("title =" + std::string(title), logDEBUG);
-			log.send("url:" + url + ", folder:" + download_folder, logDEBUG);
-		};
-	public:
-		//you'd think this method name would be better in the parent class..
-		//Returns FALSE when:
-			/* file fails to download
-			 * when we fail to update the node*/
-		/*Returns TRUE when:
-		 	* file is successfully downloaded & title=NULL or we update history */
-		//what do we do when the file downloads successfully, but we fail to update the history? throw an exception & let the caller handle fixing the XML?
-		bool download() {
-			log.send("download()", logTRACE);
-			if(!this->fetch()) return false;
-			log.send("downloaded file", logTRACE);
-
-			if (allocated_title == NULL) return true;
-			log.send("allocated_title != NULL ==> updating history", logTRACE);
-			log.send("allocated_title: " + std::string(allocated_title), logDEBUG);
-			//text-nodes are nodes themself.... this the additional .first_node() call
-			//https://stackoverflow.com/questions/62785421/c-rapidxml-traversing-with-first-node-to-modify-the-value-of-a-node-in-an
-			associated_config.first_node("history")->first_node()->value(allocated_title);//config_document.allocate_string(entry_title.c_str()));
-			log.send("Updated history", logTRACE);
-			return true;
-		};
-	private:
-		logger& log;
-		const char* allocated_title;
-		rx::xml_node<>& associated_config;
-};
+//class rssContents : private download_base {
+//	public:
+////download_base(logger &log, std::string url,std::string download_path, std::string name_of_file = "") :
+//		rssContents(rx::xml_node<> &config, char* title, std::string url, std::string download_folder) :
+//			download_base(url, download_folder),
+//			log(logger::getInstance()),
+//			allocated_title(title),
+//			associated_config(config)
+//		{
+//			log.send("rssContents constructor", logTRACE);
+//			if (title != NULL)
+//				log.send("title =" + std::string(title), logDEBUG);
+//			log.send("url:" + url + ", folder:" + download_folder, logDEBUG);
+//		};
+//	public:
+//		//you'd think this method name would be better in the parent class..
+//		//Returns FALSE when:
+//			/* file fails to download
+//			 * when we fail to update the node*/
+//		/*Returns TRUE when:
+//		 	* file is successfully downloaded & title=NULL or we update history */
+//		//what do we do when the file downloads successfully, but we fail to update the history? throw an exception & let the caller handle fixing the XML?
+//		bool download() {
+//			log.send("download()", logTRACE);
+//			if(!this->fetch()) return false;
+//			log.send("downloaded file", logTRACE);
+//
+//			if (allocated_title == NULL) return true;
+//			log.send("allocated_title != NULL ==> updating history", logTRACE);
+//			log.send("allocated_title: " + std::string(allocated_title), logDEBUG);
+//			//text-nodes are nodes themself.... this the additional .first_node() call
+//			//https://stackoverflow.com/questions/62785421/c-rapidxml-traversing-with-first-node-to-modify-the-value-of-a-node-in-an
+//			associated_config.first_node("history")->first_node()->value(allocated_title);//config_document.allocate_string(entry_title.c_str()));
+//			log.send("Updated history", logTRACE);
+//			return true;
+//		};
+//	private:
+//		logger& log;
+//		const char* allocated_title;
+//		rx::xml_node<>& associated_config;
+//};
 //checks if a folder exists, if not it attempts to create the folder. returns true on success
 bool createFolderIfNotExist(fs::path folder) {
 	static logger& log = logger::getInstance();
@@ -330,13 +326,88 @@ static void signal_handler(int signal) {
 	std::cout << "signal(" << signal << ") received: quitting\n";
 	exit(EXIT_FAILURE);
 }
+class feed : private download_base {
+	public:
+		feed(rx::xml_node<>& config_ptr, std::string fileName, std::string url, char* regex, std::string history):
+			download_base(url, RSS_FOLDER + fileName),
+			config_ref(config_ptr),
+			log(logger::getInstance()),
+			feedHistory(history),
+			regexpression(regex)
+		{
+			log.send("feed constructor",logTRACE);
+			log.send("NAME:" + this->getPath().string());
+			log.send("URL:" + url, logDEBUG);
+			log.send("HISTORY:" + feedHistory);
+			if (!fetch()) throw std::runtime_error("Failed to download file");
+		};
+		void parse() {
+			if (doneParsing) return;
+		//2. parse the FEED
+			rx::xml_document<> feed;
+			rx::file<char> feedFile(getPath());
+			try {
+				log.send("Parsing RSS", logTRACE);
+				feed.parse<rx::parse_no_data_nodes>(feedFile.data());
+			} catch(rx::parse_error &e) {
+				log.send("Failed to parse" + std::string(e.what()) + "\nskipping...", logERROR);
+				feed.clear();
+				throw std::runtime_error("Failed to parse file");
+			}
+		//3. Store download links & names to config
+			log.send("Regular Expression:" + std::string(regexpression), logDEBUG);
+			log.send("Begin fetching downloads", logTRACE);
+			for (auto *node = feed.first_node()->first_node()->first_node("item");
+					node;
+					node = node->next_sibling()) {
+				std::string entry_title = node->first_node("title")->value();
+				log.send("ENTRY TITLE: " + entry_title, logDEBUG);
+				std::string entry_url = node->first_node("link")->value();
+				log.send("URL: " + entry_url, logDEBUG);
+				if (entry_title.compare(feedHistory) == 0) {
+					log.send("Download in history. skipping...", logTRACE);
+					break; //because it is in chronological order, we can just stop here
+				}
+				if (match_title(regexpression, entry_title)) {
+					if (newHistory == false) {
+						newHistory = true;
+						newHistoryTitle = entry_title;
+					}
+					download_base tmp(entry_url, fs::path(DOWNLOAD_FOLDER + url_to_filename(entry_url)));
+					downloads.push_back(std::move(tmp));
+					log.send("Added " + entry_title + " to downloads");
+				}
+			}
+			feed.clear();
+			doneParsing = true;
+		}
+		//returns the pointer to the xml child in the config relating to this feed
+		const rx::xml_node<>& getConfigRef() {
+			return config_ref;
+		}
+		//if downloads != empty, then the history received MUST be new
+		const std::string getHistory() {
+			if (newHistory) return newHistoryTitle;
+			else return feedHistory;
+		}
+		const std::vector<download_base> getDownloads() {
+			return downloads;
+		}
+	private:
+		const rx::xml_node<>& config_ref;
+		logger& log;
+	private:
+		bool newHistory = false;
+		std::string newHistoryTitle;
+		const std::string feedHistory;
+		const char* regexpression;
+		std::vector<download_base> downloads;
+		//prevent the main function from being run twice
+		bool doneParsing = false;
+};
+
 int main(void) {
-	static logger &log = logger::getInstance(logWARNING);
-	//signal handlers that ensure we properly deconstruct our static variables on exit
-	std::signal(SIGSEGV, signal_handler);
-	std::signal(SIGINT, signal_handler);
-	//
-	log.send("Starting RSS-Feed:");
+	static logger &log = logger::getInstance(logDEBUG);
 	if (!fs::exists(CONFIG_NAME)) {
 		std::cout << "PLEASE POPULATE: " << CONFIG_NAME << "\n";
 		log.send("CONFIG: " + std::string(CONFIG_NAME) + "does not exist", logERROR);
@@ -351,6 +422,9 @@ int main(void) {
 		log.send("Quitting...");
 		exit(EXIT_FAILURE);
 	}
+	//signal handlers that ensure we properly deconstruct our static variables on exit
+	std::signal(SIGSEGV, signal_handler);
+	std::signal(SIGINT, signal_handler);
 	//loads the files... they both must live together
 	static rx::xml_document<> config_document;
 	static rx::file<> config_file(CONFIG_NAME);
@@ -363,82 +437,57 @@ int main(void) {
 		exit(EXIT_FAILURE);
 	}
 	//pointers to each config node
-	std::vector<rx::xml_node<char>*> config_nodes;
+	std::vector<feed> feeds;
+	int count = 0;
 	log.send("Collect entries from the config", logTRACE);
 	for (auto item_node = config_document.first_node()->first_node("item");
 			item_node;
 			item_node = item_node->next_sibling()) {
-		log.send(" * Adding config node", logDEBUG);
-		config_nodes.push_back(item_node);
-	}
-	//downloads in format of: NAME, URL
-	std::vector<rssContents> download_links;
-{//begin-scope
-	log.send("Checking Feeds:");
-	for (auto config : config_nodes) {
-	//1. download its linked RSS feed & set some data
-		std::string configFeedName = config->first_node("feedFileName")->value();
-		std::string url = config->first_node("feed-url")->first_node()->value();
-		std::string feedHistory = config->first_node("history")->value();
-
-		log.send("----------");
-		log.send("NAME:" + configFeedName);
-		log.send("URL:" + url, logDEBUG);
-		log.send("HISTORY:" + feedHistory);
-
-		download_entry config_download(url, RSS_FOLDER, configFeedName);
-		if(!config_download.fetch()) {
-			log.send("Failed to download file. SKIPPING...", logERROR);
-			continue;
-		}
-		//
-		//
-	//2. parse the FEED
-		rx::xml_document<> feed;
-		rx::file<char> feedFile(config_download.getFileName());
+		std::string configFeedName = item_node->first_node("feedFileName")->value();
+		std::string url = item_node->first_node("feed-url")->first_node()->value();
+		std::string feedHistory = item_node->first_node("history")->value();
+		char* regexpression = item_node->first_node("expr")->value();
 		try {
-			log.send("Parsing RSS", logTRACE);
-			feed.parse<rx::parse_no_data_nodes>(feedFile.data());
-		} catch(rx::parse_error &e) {
-			log.send("Failed to parse" + std::string(e.what()) + "\nskipping...", logERROR);
-			feed.clear();
-			continue;
+			feeds.push_back(feed(*item_node, configFeedName, url, regexpression, feedHistory));
+			log.send("Added feed() to vector", logTRACE);
+			if(count++ == 3) break;
+		} catch(std::runtime_error &e) {
+			log.send(e.what(), logERROR);
 		}
-	//3. Store download links & names to config
-		bool first = true;
-		char* regexpression = config->first_node("expr")->value();
-		log.send("Regular Expression:" + std::string(regexpression), logDEBUG);
-		log.send("Begin fetching downloads", logTRACE);
-		for (auto *node = feed.first_node()->first_node()->first_node("item");
-				node;
-				node = node->next_sibling()) {
-			std::string entry_title = node->first_node("title")->value();
-			log.send("ENTRY TITLE: " + entry_title, logDEBUG);
-			std::string entry_url = node->first_node("link")->value();
-			log.send("URL: " + entry_url, logDEBUG);
-			if (entry_title.compare(feedHistory) == 0) {
-				log.send("Download in history. skipping...", logTRACE);
-				break; //because it is in chronological order, we can just stop here
+	}
+	std::vector<std::thread> parsing_feeds;
+	//parse each feed in a thread
+	count = 0;
+	for (auto current_feed : feeds) {
+		log.send("creating thread",logTRACE);
+		auto t = std::thread(&feed::parse,(current_feed));
+		parsing_feeds.push_back(std::move(t));
+		if(count++ == 3)break;
+	}
+	//join threads
+	log.send("JOINING THREADS!!\n\n\n");
+	for (auto& t : parsing_feeds)
+		t.join();
+	//sequentially download each file
+	for (auto current_feed : feeds) {
+		//current_feed.parse();
+		auto files = current_feed.getDownloads();
+		if (!files.empty()) {
+			for (auto file : files) {
+				if (!file.fetch())
+					log.send("Failed to download file");
+				else
+					log.send("downloaded file");
 			}
-			if (match_title(regexpression, entry_title)) {
-				if (first) {
-					download_links.push_back(rssContents(*config, config_document.allocate_string(entry_title.c_str()), entry_url, DOWNLOAD_FOLDER));
-					first = false;
-				}
-				else download_links.push_back(rssContents(*config, NULL, entry_url, DOWNLOAD_FOLDER));
-				log.send("Added " + entry_title + " to downloads");
-			}
+			//download files
 		}
-		feed.clear();
+		//update history
 	}
-}//end-scope
-	if (download_links.empty()) {
-		log.send("No Downloads. Quitting...");
-		exit(EXIT_SUCCESS);
-	}
-	log.send("<--" + std::to_string(download_links.size()) + " files to download-->");
-	for (auto download : download_links) 
-		download.download(); 
+	
+	
+	//
+	log.send("reminder to re-enable config update");
+	exit(EXIT_SUCCESS);
 	//update the config
 	log.send("Saving the updated config file", logTRACE);
 	std::ofstream new_xml(CONFIG_NAME);
